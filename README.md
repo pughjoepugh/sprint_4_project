a0549d216bfe078f7a9da1a4b842f0418477fd47
pughjoepugh <83442296+pughjoepugh@users.noreply.github.com>
2024-12-14 21:20:59 -0800

    added sombrero, requirements, and toml file

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # sprint_4_project
+_________
+### Introduction:<br>
+The purpose of this exercise is to analyze used car sales data from the United States and produce a web app to share findings. First, the data will be loaded, then opened and inspected in order to understand the structure and potential issues. Next is processing the data. By making necessary or useful updates, the data will be prepared for further data exploration.
+
+Preprocessing the data will consist of two parts. The first part is cleaning the data. This will ensure the data is accurate and consistent. Techniques involved are:
+
+Filling Missing Values: Data points that are missing will be filled in to avoid errors. For quantitative data, an average from similiar data will be utilized; for qualitative data points, the most common entry among similiar data.
+
+Standardizing Values: Data entries may have different spelling or formats from eachother while meaning to express the same value. An example that comes up is (f-150, f150, f_150 superduty)
+
+Correcting Data Types: Ensure each column of data is in the correct format, such that numbers are numbers, and dates are dates.
+
+Feature Engineering: Creation of new data columns that might be useful, like extracting the manufacturer from a model column.
+
+Most of the preprocessing will be done in the preprocessing notebook ('PreProcessing.ipynb'). Converting dates to datetime format as well as the creation of a second dataframe storing sales data, will be conducted in the EDA notebook ('EDA.ipynb')
+
+At the end of preprocessing, the state of the data has been saved as: processed_vehicles.csv
+After the creation of the sales dataframe, it was stored in the root directory as: vehicle_sales.csv
+The original data file is located in the root directory and is titled 'vehicles_us.csv'
+
+### EDA
+___
+Most of the preprocessing was be done in the preprocessing notebook ('PreProcessing.ipynb'). Converting dates to datetime format as well as the creation of a second dataframe storing sales data, will be conducted in the EDA notebook ('EDA.ipynb')
+
+A second dataframe will be constructed with data from the pre-processed dataframe. The second dataframe will focus on sales. columns will be date, sales_in_units, sales_in_dollars, inventory_in_units, inventory_in_dollars (based on sale price). The original dataframe had listing dates and how many days the vehicle was for sale. I used this information to create a sale date. And anytime time between listing and sold would refer to a time when the vehicle was in the inventory.
+
+Analyzing the Data: <br.>
+In this notebook will be several plots created in order to further investigate the data.  Plotly.express is the visualization library utilized. 
+
+Statistical Analysis: <br>
+Various statistical calculations will be made in order to interpret data and draw conclusions, identifying trends or correlations.
+
+### Insights & conclusions
+___
+The primary take away I got from exploring the data was inventory size. Over the course of the dataset, vehicles are listed and sold. The inventory starts at zero and climbs all the way to 6000 vehicles. The inventory will hover around 6000 until they the last day of listing. From then vehicles are only sold off.
+
+Assumptions being made: 
+each vehicle takes up the same space/ parking spot.
+vehicles are being sold on a percent markup. Purchase price of vehicles was not provided, only the listing price. (not technically the sale price)
+Inventory was a limiting factor, maybe it wasn't
+
+Inventory size is important because certain vehicles traits have different listing durations. Example: hatchback vehicles have a below average sale price, and an above average listing duration. Hatchbacks generate less revenue per vehicle. 
+
+Over the datasets timeframe they sold $625,125,255 in vehicles. I do not work in car sales. But that sounds like a congratulations!
+
+### Tools
+___
+Tools being used during the analysis are:
+
+Pandas: <br>
+Library with tools used for data manipulation and analysis. All tables being displayed are in the pandas dataframe format. This is the primary tool used for cleaning, transforming, and aggregating the dataset.
+
+NumPy: <br>
+A library designed to handles numerical support across larger datasets.
+
+Streamlit:<br>
+A library used to build web applications with an emphasis on data science. Streamlit allows for creation of interactive applications to share data insights
+
+Plotly Express:<br>
+A data visualization library used to create interactive plots and visualizations.
+
+Datetime:<br>
+Datetime is a module used to interupt, handle, and change date or time data.
+
+Render:<br>
+A web application hosting site. Render will be linked to a gitHub account hosting this project/ repository.
+
+GitHub:<br>
+A web-based platform that allows users to store, share, and manage code. Github is also a service that facilitates version control, allowing multiple contributers to make changes, while offering the means to correct potential mistakes.
+
+VScode:<br>
+IDE or coding platform used to in creation of this project.
---
+++ app.py
@@ -0,0 +1,300 @@
+# import libraries
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# load dataframes
+vehicles = pd.read_csv('processed_vehicles.csv')
+sales = pd.read_csv('vehicle_sales.csv')
+
+# display both dataframes
+st.image("sombrero.png")
+st.header('Used car sales data')
+st.markdown("""
+This app examines used car sales data and was built using
+**Python libraries:** Pandas, Streamlit, plotly.express
+
+The data can be viewed below:""")
+st.markdown('vehicles.csv')
+st.dataframe(vehicles)
+
+st.markdown("sales.csv")
+st.dataframe(sales)
+
+# LINE GRAPH
+# plot total sales per day
+
+# every vehicle sold value total
+sum_sales = sales.sales_dollars.sum()
+
+# header
+st.subheader('Sales')
+st.markdown(
+    f"The data is from May 2018 - April 2019. The total vehicle sales amounted to: ${sum_sales:,}")
+
+# select box to choose y variable
+feature = st.selectbox('Feature', ['sales_dollars', 'sales_in_units',
+                                   'inventory_in_dollars', 'inventory_in_units'])
+
+# line plot of sales over time
+line_fig = px.line(sales,
+                   x='index',
+                   y=feature)
+
+line_fig.update_layout(xaxis_title="Time in day intervals",
+                       yaxis_title=f"Sum of {feature}",
+                       width=800,
+                       height=600,
+                       )
+
+# retrieve last listing date for feature addition
+last_listing = vehicles['date_posted'].max()
+
+# Add a vertical line for average price
+line_fig.add_shape(type="line",
+                   x0=last_listing, x1=last_listing,
+                   y0=0, y1=sales[feature].max()+50,
+                   line=dict(color='red', dash='dash'))
+
+# add text annotation for the line
+line_fig.add_annotation(
+    x=last_listing,
+    y=(sales[feature].max() + 10),
+    text=f"Last listing date: {last_listing}",
+    showarrow=True,
+    arrowhead=1,
+    ax=40,
+    ay=-40,
+    bordercolor="black",
+    borderwidth=1,
+    borderpad=4,
+    opacity=1,
+)
+
+st.plotly_chart(line_fig)
+
+# BAR PLOT
+# average listing duration of vehicle make (how fast vehicle makes sell)
+
+# header
+st.subheader('Average listing duration for vehicle manufacturers')
+st.markdown('How fast vehicles sell (brand / type / color)')
+
+# create a select box for car colors
+sorted_color = sorted(vehicles['paint_color'].unique())
+selected_color = st.multiselect(
+    'Vehicle color', sorted_color, sorted_color[:1])
+
+# create a select box for car type
+sorted_type = sorted(vehicles['type'].unique())
+selected_type = st.multiselect(
+    'Vehicle type', sorted_type, sorted_type[9:10])
+
+# calculate the average listing duration by vehicle make and or type
+average_make = vehicles[
+    (vehicles['paint_color'].isin(selected_color)) &
+    (vehicles['type'].isin(selected_type))].groupby(
+    'make')['days_listed'].mean().reset_index()
+
+# bar plot
+bar_fig = px.bar(average_make,
+                 x='make',
+                 y='days_listed')
+
+# order the bars by ascending height
+bar_fig.update_layout(xaxis={'categoryorder': 'total ascending'})
+
+# add titles/ labels
+bar_fig.update_layout(
+    xaxis_title="Vehicle make",
+    yaxis_title="Average listing duration (days)",
+    width=800,
+    height=575,
+)
+
+# add a horizontal line
+bar_fig.add_shape(
+    type="line",
+    x0=-1, x1=len(vehicles['make'].unique()) - 0.5,
+    y0=vehicles['days_listed'].mean(), y1=vehicles['days_listed'].mean(),
+    line=dict(color='Red', width=2, dash='dash'),
+)
+
+# Add an annotation if desired
+bar_fig.add_annotation(
+    x=5,  # horizontal position within the plot area
+    y=45,
+    text=f'Average duration of all vehicles: {
+        vehicles['days_listed'].mean():.2f} days',
+    showarrow=False,
+    yshift=10,  # vertical shift
+    bordercolor="black",
+    borderwidth=1,
+    opacity=0.8
+)
+
+# automatically adjust text size, format, and position
+bar_fig.update_traces(texttemplate='%{y:.2f}', textposition='inside')
+
+# Show the plot
+st.write(bar_fig)
+
+# BAR2
+# average price of vehicle type
+
+st.subheader("Vehicle average listing price ($)")
+st.markdown('Select which features to few')
+
+# create a select box for car colors
+sort_color = sorted(vehicles['paint_color'].unique())
+select_color = st.multiselect(
+    'Vehicle color', sort_color, sort_color, key='1')
+
+# create a select box for car type
+sort_type = sorted(vehicles['type'].unique())
+select_type = st.multiselect(
+    'Vehicle type', sort_type, sort_type, key='2')
+
+# calculate the average listing duration by vehicle make and or type
+average_price = vehicles[
+    (vehicles['paint_color'].isin(select_color)) &
+    (vehicles['type'].isin(select_type))].groupby(
+    'make')['price'].mean().reset_index()
+
+# bar plot
+fig3 = px.bar(average_price,
+              x='make',
+              y='price',
+              text='price')
+
+# automatically adjust text size, format, and position
+fig3.update_traces(texttemplate='%{y:,.2f}', textposition='inside')
+
+# order the bars by descending height
+fig3.update_layout(xaxis={'categoryorder': 'total descending'})
+
+# add titles/ labels
+fig3.update_layout(
+    xaxis_title="Vehicle type",
+    yaxis_title="Average listing price ($)",
+    width=800,
+    height=575,
+)
+
+# add a horizontal line
+fig3.add_shape(
+    type="line",
+    # extend line horizontally across the plot
+    x0=-0.5, x1=len(vehicles['make'].unique()) - 0.5,
+    # set at average_value
+    y0=vehicles['price'].mean(), y1=vehicles['price'].mean(),
+    line=dict(color='Red', width=2, dash='dash'),  # Style of the line
+)
+
+# add an annotation if desired
+fig3.add_annotation(
+    x='van',  # horizontal position within the plot area
+    y=vehicles['price'].mean()+100,
+    text=f'Average price: ${vehicles['price'].mean():,.2f}',
+    showarrow=False,
+    yshift=10,  # vertical shift
+    bordercolor="black",
+    borderwidth=1,
+    opacity=0.8
+)
+# Show the plot
+st.plotly_chart(fig3)
+
+
+# SCATTER
+st.subheader('Comparing price with release year of used cars')
+
+# slider to select years
+years = st.slider("Select a range of years", 1929, 2019, (2000, 2010))
+st.write("Model years:", years)
+st.markdown(
+    'Once year range has been selected, please auto-scale the plot (6th icon to the right)')
+
+# unpack the tuple
+early, later = years
+
+# filter range selection
+time_zone = vehicles[(vehicles['model_year'] >= early) &
+                     (vehicles['model_year'] <= later)]
+
+# explore data
+fig4 = px.scatter(time_zone,
+                  x='model_year',
+                  y='price',
+                  color='condition',
+                  category_orders={'condition': [
+                      'salvage', 'fair', 'good', 'excellent', 'like new', 'new']}
+                  )
+fig4.update_layout(
+    xaxis_title="Vehicle production year",
+    yaxis_title="Price of vehicle ($)",
+    width=800,
+    height=575,
+)
+fig4.update_traces(opacity=0.5)
+# Set x and y axis limits
+fig4.update_xaxes(range=[vehicles['model_year'].min() -
+                         1, vehicles['model_year'].max()+1])
+fig4.update_yaxes(range=[0, 100000])
+
+st.write(fig4)
+
+# HISTO
+st.subheader('Used car prices')
+
+# create a select box for car conditions
+sort_condition = sorted(vehicles['condition'].unique())
+select_condition = st.multiselect(
+    'Vehicle condition', sort_condition, sort_condition, key='99')
+
+# create grouping
+# calculate the average price by condition
+average_condition = vehicles.groupby('condition')['price'].mean().reset_index()
+
+# calculate the average price by condition
+conditions = vehicles[(vehicles['condition'].isin(select_condition))]
+
+# histogram plot
+fig5 = px.histogram(conditions,
+                    x='price',
+                    nbins=230,
+                    color='condition',
+                    barmode='overlay',
+                    )
+
+# add an annotation
+fig5.add_annotation(x=80000,
+                    y=3250,
+                    text=f"""Average prices:<br>
+                    {average_condition.iloc[0, 0]}: ${average_condition.iloc[0, 1]:,.2f}<br>
+                    {average_condition.iloc[1, 0]}: ${average_condition.iloc[1, 1]:,.2f}<br>
+                    {average_condition.iloc[2, 0]}: ${average_condition.iloc[2, 1]:,.2f}<br>
+                    {average_condition.iloc[3, 0]}: ${average_condition.iloc[3, 1]:,.2f}<br>
+                    {average_condition.iloc[4, 0]}: ${average_condition.iloc[4, 1]:,.2f}<br>
+                    {average_condition.iloc[5, 0]}: ${average_condition.iloc[5, 1]:,.2f}""",
+                    showarrow=False,
+                    bordercolor="black",
+                    borderwidth=1,
+                    opacity=0.8,
+                    align="left"
+                    )
+
+fig5.update_layout(xaxis_title="Price ($)",
+                   yaxis_title="Number of vehicles",
+                   width=800,
+                   height=575,
+                   )
+
+# Change transparency/visibility with 'opacity' parameter
+fig5.update_traces(opacity=0.6)
+
+# set x and y axis limits
+fig5.update_xaxes(range=[0, 100000])
+fig5.update_yaxes(range=[0, len(conditions['condition'])/10])
+
+st.write(fig5)
